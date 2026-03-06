625ac0d64c2f654e483d0ec8bd35a1d746225e7f
next-007 <p0lar1s@naver.com>
2026-03-06 15:48:37 +0900

    list, regist 완료

--- react_todo_front/src/App.jsx
+++ react_todo_front/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import Header from "./components/commons/Header";
+import { Routes, Route } from "react-router-dom";
+import TodoListPage from "./pages/TodoListPage";
+import TodoRegistPage from "./pages/TodoRegistPage";
 
 function App() {
   return (
     <div>
-      <h1>메인페이지</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<TodoListPage />} />
+        <Route path="/regist" element={<TodoRegistPage />} />
+      </Routes>
     </div>
   );
 }
